b6e92c8c57dcc3408e0021534a749eea2b9fdf09
abdurrehman <abdirrehman.bse@gmail.com>
2024-07-31 11:38:27 +0200

    added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project with LangChain LLMs VectorDB Streamlit',
+    version= '0.0.0',
+    author= 'Abdurrehman',
+    author_email= 'abdurrehman.bse@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
